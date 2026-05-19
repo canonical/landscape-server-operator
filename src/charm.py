5b65224ca8cbf7634ef22757b4c3a2916b31d21b
Spencer Runde <spencer.runde@canonical.com>
2026-05-19 17:17:52 -0500

    feat: use 1/stable for default outbox track

--- src/charm.py
+++ src/charm.py
@@ -118,7 +118,7 @@ LANDSCAPE_PACKAGES = (
     "landscape-common",
 )
 LANDSCAPE_OUTBOX_SNAP = "landscape-outbox"
-DEFAULT_OUTBOX_SNAP_CHANNEL = "latest/stable"
+DEFAULT_OUTBOX_SNAP_CHANNEL = "1/stable"
 LANDSCAPE_UBUNTU_INSTALLER_ATTACH = "landscape-ubuntu-installer-attach"
 
 DEFAULT_SERVICES = (
