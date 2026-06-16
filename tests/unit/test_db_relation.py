01f8a33292f2e7b9b392896b9d6b9e4b0eb94113
jansdhillon <jan.dhillon@canonical.com>
2026-06-16 17:50:46 -0600

    rm slop

--- tests/unit/test_db_relation.py
+++ tests/unit/test_db_relation.py
@@ -19,14 +19,6 @@ def check_call():
         yield p
 
 
-@pytest.fixture(autouse=True)
-def schema_supports_flag():
-    with patch.object(
-        LandscapeServerCharm, "_schema_supports_flag", return_value=False
-    ):
-        yield
-
-
 @pytest.mark.parametrize("event", ("relation_changed", "relation_joined"))
 class TestDBRelation:
     """
