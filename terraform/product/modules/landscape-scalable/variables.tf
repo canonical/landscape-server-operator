fc1816b1ac38abccc487d25d6a02eff9fc171539
jansdhillon <jan.dhillon@canonical.com>
2026-06-19 11:24:30 -0600

    bump ls defaults to 26.04/edge chan and self-hosted-beta PPA

--- terraform/product/modules/landscape-scalable/variables.tf
+++ terraform/product/modules/landscape-scalable/variables.tf
@@ -9,11 +9,11 @@ variable "landscape_server" {
   description = "Configuration for the Landscape Server charm."
   type = object({
     app_name   = optional(string, "landscape-server")
-    channel    = optional(string, "26.04/beta")
+    channel    = optional(string, "26.04/edge")
     charm_name = optional(string, "landscape-server")
     config = optional(map(string), {
       autoregistration               = "true"
-      landscape_ppa                  = "ppa:landscape/self-hosted-26.04"
+      landscape_ppa                  = "ppa:landscape/self-hosted-beta"
       min_install                    = "true"
       root_url                       = "https://landscape.local/"
       enable_hostagent_messenger     = "true"
